3f5720e0d5b779f7f3573d41e3f04dd0519417cf
Shubh03singh <128495004+Shubh03singh@users.noreply.github.com>
2023-03-21 23:26:41 +0530

    Create i_in_range(for).py

---
+++ i_in_range(for).py
@@ -0,0 +1,9 @@
+   #with increament by 1
+for i in range(2,9):
+  print(i) 
+  
+    #with increament by userdefined value
+    
+a=int(input("Enter the value of a:"))
+for i in range(2,90,a):
+  print(i)
